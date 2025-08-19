30ace680f73df9dd70df231b84c010d9b5720454
Bradley <157209409+BradleyXiX@users.noreply.github.com>
2025-08-19 19:06:12 +0300

    Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import Navbar from './sections/navbar'
+import Hero from './sections/Hero'
 
 const App = () => {
   return (
     <div className='container mx-auto max-w-7xl'>
       <Navbar />
+      <Hero />
+      <section className='min-h-screen'></section>
+      <section className='min-h-screen'></section>
+      <section className='min-h-screen'></section>
+      <section className='min-h-screen'></section>
+      <section className='min-h-screen'></section>
     </div>
 
   )
