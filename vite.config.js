c51e4a76e4fde7b4201e2dfd052d0d7ff2a241fa
Bradley <157209409+BradleyXiX@users.noreply.github.com>
2025-08-24 15:20:04 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/Portfolio-2.0/',
+  plugins: [react(), tailwindcss()],
 })
