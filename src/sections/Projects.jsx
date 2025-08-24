dbaa4ed67ed2a5678a248e91d031d3584c3737eb
Bradley <157209409+BradleyXiX@users.noreply.github.com>
2025-08-24 15:58:59 +0300

    improved UX

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -23,13 +23,6 @@ const Projects = () => {
       {myProjects.map((project) => (
         <Project key={project.id} {...project} setPreview={setPreview} />
       ))}
-      {preview && (
-        <motion.img
-          className="fixed top-0 left-0 z-50 object-cover h-56 rounded-lg shadow-lg pointer-events-none w-80"
-          src={preview}
-          style={{ x: springX, y: springY }}
-        />
-      )}
     </section>
   );
 };
