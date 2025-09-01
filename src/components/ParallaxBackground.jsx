3f2394733c419dc6c5afef3af1c67bd8b63f8736
Bradley <157209409+BradleyXiX@users.noreply.github.com>
2025-09-01 21:57:41 +0300

    working on new home page

--- src/components/ParallaxBackground.jsx
+++ src/components/ParallaxBackground.jsx
@@ -1,4 +1,4 @@
-import { motion, useScroll, useSpring, useTransform } from "motion/react";
+import { motion } from "motion/react";
 
 const ParallaxBackground = () => {
   
---
+++ src/sections/Home.jsx
@@ -0,0 +1,13 @@
+import HeroText from "../components/HeroText"
+
+const Home = () => {
+  return (
+    <section className="c-space w-full min-h-screen ">
+        <HeroText />
+        
+
+    </section>
+  )
+}
+
+export default Home
