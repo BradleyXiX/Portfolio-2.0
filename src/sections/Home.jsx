626aacd4cdca4d60dd76d47c456bb4cd410bc9b0
Bradley <157209409+BradleyXiX@users.noreply.github.com>
2025-09-02 15:37:41 +0300

    new home page

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -2,9 +2,9 @@ import HeroText from "../components/HeroText"
 
 const Home = () => {
   return (
-    <section className="c-space w-full min-h-screen ">
-        <HeroText />
-        
+    <section className=" flex c-space w-full min-h-screen pt-20 ">
+      <HeroText />
+      <img src="../../public/Home-cover.png" className="w-[35vw] h-[35vw] rounded-full shadow-[0_0_25px_var(--primary)] transition-all duration-400 ease-in-out hover:shadow-[0_0_25px_var(--primary),0_0_50px_var(--primary),0_0_100px_var(--primary)]" alt="home-img" />      
 
     </section>
   )
